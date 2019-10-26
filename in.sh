d8256b9a470db75944d17fc4420c3d14958baaf8
rango <1614056450@qq.com>
2019-10-26 10:08:59 +0800

    add

--- in.sh
+++ in.sh
@@ -4,8 +4,7 @@ export XIM=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"
-
 export KDEWM=/usr/bin/i3
-#compton -cCGfF -o 0.01 -O 1 -I 1 -t 0 -l 0 -r 3 -D2 -m 1 -b
+compton -cCGfF -o 0.01 -O 1 -I 1 -t 0 -l 0 -r 3 -D2 -m 1 -b
 #xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &
-compton -m 1 -C -G --active-opacity 1.0 --backend glx -i 1.0 -e 1.0 -b
+#compton -m 1 -C -G --active-opacity 1.0 --backend glx -i 1.0 -e 1.0 -b
